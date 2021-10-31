5b2b2ff39a4dfbfd28f141cfa55ce4565851ff6a
Andrea <montanari.andrea98@gmail.com>
2021-10-31 17:17:42 +0100

    Added motion sensor in RT sw

--- FreeRTOS_Alarm/FreeRTOS_Alarm.ino
+++ FreeRTOS_Alarm/FreeRTOS_Alarm.ino
@@ -2,22 +2,31 @@
 #include <semphr.h>
 #include <Keypad.h>
 
+#define INCLUDE_vTaskSuspend 1
+
 #define PIN 1
 #define STAMP 0
+#define ALARM_TRIGGERED 3
 #define LENGTH_PIN 4
+
+#define MOTION_SENSOR_PIN 31
 
 SemaphoreHandle_t mutex = NULL;
 SemaphoreHandle_t s_pin = NULL;
 SemaphoreHandle_t s_stamp = NULL;
+SemaphoreHandle_t s_motion_sensor = NULL;
 
 void taskPin(void *pvParameters);
 void taskStamp(void *pvParameters);
+void taskMotionSensor(void* pvParameters);
 
 struct gestore
 {
   int stato;
   bool alarm;          // mode: DISABLED/ENABLED
   bool siren;         // ON/OFF per far suonare l'allarme
+  bool alarm_triggered;
+  bool b_motion_sensor;
 }g;
 char user_pin[LENGTH_PIN];
 int index_pin; // per scorrere array pin
@@ -25,13 +34,21 @@ char true_system_pin[4] = {'0', '0', '0', '0'};
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
+
+int movement_sensor_value; // Place to store read PIR Value
+
 char hexaKeys[ROWS][COLS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
-byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+    
+//byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+//byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+byte rowPins[ROWS] = {39, 41, 43, 45}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {47, 49, 51, 53}; //connect to the column pinouts of the keypad
+
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 int b_pin, b_stamp;
@@ -81,12 +98,14 @@ void print_alarm_state(){
  */
 void start_pin(void *pvParameters)
 {
+	// Perché (TickType_t)100?
     xSemaphoreTake(mutex, (TickType_t)100);
     //Serial.println("Sono lo start_pin");
     if (g.stato == PIN)
     {
         xSemaphoreGive(s_pin);
     }
+	// Perché bloccarla?
     else
         b_pin++;
     xSemaphoreGive(mutex);
@@ -148,8 +167,21 @@ void stamp()
             Serial.println(user_pin);
             xSemaphoreTake(mutex, (TickType_t)100);
             // il pin è giusto, quindi bisogna cambiare lo stato dell'allarme: nel caso in cui sia off->on nel caso sia on->off
-            if (g.alarm)g.alarm=false;              //anche qui g.alarm è una var condivisa, quindi possibili race condition
-            else g.alarm=true;
+			if (g.alarm) {
+				g.alarm = false;              //anche qui g.alarm è una var condivisa, quindi possibili race condition
+			}
+			else if (g.alarm_triggered) { // Impostare anche l'allarme ad off ?
+				g.alarm_triggered = false;
+				Serial.println("Allarme spento.");
+			}
+			else {
+				g.alarm = true;
+				if (g.b_motion_sensor) {	// Sblocca il sensore di movimento se era bloccato
+					g.b_motion_sensor = false;
+					xSemaphoreGive(s_motion_sensor);
+					Serial.println("Attivo sensore movimento."); // Dopo aver attivato il sensore non risponde più agli input da tastierino (?)
+				}
+			}
             xSemaphoreGive(mutex);
 
         }
@@ -169,8 +201,8 @@ void stamp()
 
 void end_stamp(void *pvParameters)
 {
-    xSemaphoreTake(mutex, (TickType_t)100);
-    //Serial.println("Sono lo end_stamp");
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    Serial.println("Sono l'end_stamp");
     g.siren = PIN;
     if (b_pin)
     {
@@ -180,14 +212,50 @@ void end_stamp(void *pvParameters)
     xSemaphoreGive(mutex);
 }
 
+
+
+void start_motion_sensor(void* pvParameters)
+{
+	xSemaphoreTake(mutex, portMAX_DELAY);
+	if (g.alarm || g.alarm_triggered)
+	{
+		xSemaphoreGive(s_motion_sensor);
+		//Serial.println("Sensore di movimento parte.");
+	}
+	else {
+		Serial.println("Sensore di movimento si BLOCCA.");
+		g.b_motion_sensor = true;
+	}
+	xSemaphoreGive(mutex);
+	xSemaphoreTake(s_motion_sensor, portMAX_DELAY); // mi blocco qui nel caso
+}
+
+void motion_sensor()
+{
+	movement_sensor_value = digitalRead(MOTION_SENSOR_PIN);
+	if (movement_sensor_value) {
+		// digitalWrite(ledPin, movement_sensor_value);
+		g.alarm_triggered = true;
+		Serial.println("\nMovimento rilevato!!!\n");
+	}
+
+}
+
+
+
 void setup()
 {
+	pinMode(MOTION_SENSOR_PIN, INPUT);
+
     Serial.begin(9600);
     Serial.println("Inizio il setup");
 
     g.stato = PIN;
     g.alarm=false;
     g.siren=false;
+	g.alarm_triggered = false;
+	g.b_motion_sensor = false;
+
     // inizializzo i pin
     for (int k = 0; k > LENGTH_PIN; k++)
     {
@@ -201,6 +269,7 @@ void setup()
     //print_user_pin();
     b_pin = b_stamp = 0;
     
+	// Credo che i controlli si possano togliere (?)
     if (mutex == NULL) // Check to confirm that the Serial Semaphore has not already been created.
     {
         mutex = xSemaphoreCreateMutex();
@@ -213,6 +282,8 @@ void setup()
     {
         s_stamp = xSemaphoreCreateBinary();
     }
+	s_motion_sensor = xSemaphoreCreateBinary();
+
     xTaskCreate(
         taskStamp, "task-stamp", 128, NULL, 1 // priority
         ,
@@ -222,6 +293,11 @@ void setup()
         taskPin, "task-pin", 128, NULL, 10 // priority
         ,
         NULL);
+
+	xTaskCreate(
+		taskMotionSensor, "task-motion-sensor", 128, NULL, 10 // priority
+		,
+		NULL);
         
 }
 
@@ -232,9 +308,9 @@ void taskStamp(void *pvParameters) // This is a task.
     for (;;)
     {
         start_stamp(pvParameters);
-        vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
+        //vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
         stamp();
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        //vTaskDelay(100 / portTICK_PERIOD_MS);
         end_stamp(pvParameters);
     }
 }
@@ -246,11 +322,24 @@ void taskPin(void *pvParameters)
     for (;;)
     {
         start_pin(pvParameters);
-        vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
+        //vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
         get_pin();
-        vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
+       //vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
         end_pin(pvParameters);
     }
+}
+
+void taskMotionSensor(void* pvParameters)
+{
+	(void)pvParameters;
+	//Serial.begin(4800);
+	for (;;)
+	{
+		start_motion_sensor(pvParameters);
+		//vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
+		motion_sensor();
+		//vTaskDelay(100 / portTICK_PERIOD_MS); // wait for one second
+	}
 }
 
 void loop()
