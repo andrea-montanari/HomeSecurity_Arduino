99a43d8b2f0775a7ae715cb54e006a0b9b42a9c9
Andrea <montanari.andrea98@gmail.com>
2021-11-14 19:45:13 +0100

    Porting to ESP32 and code cleaning

--- FreeRTOS_Alarm/FreeRTOSConfig.h
+++ FreeRTOS_Alarm/FreeRTOSConfig.h
@@ -1,11 +1,13 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
+#define portNUM_PROCESSORS 1
+
 #define configUSE_PREEMPTION                    1
 #define configUSE_TIME_SLICING                  1
 #define configIDLE_SHOULD_YIELD                 1
 #define configMINIMAL_STACK_SIZE                128
-#define configCPU_CLOCK_HZ                      16000000
+#define configCPU_CLOCK_HZ                      2400000000
 #define configTICK_RATE_HZ                      250
 #define configTOTAL_HEAP_SIZE                   10240
 #define configMAX_PRIORITIES                    3
